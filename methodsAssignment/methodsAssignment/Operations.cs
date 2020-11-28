89d15fe15a005f596966f78fe9b02b12d5f51bfa
Cade Wilson <cade25wilson@gmail.com>
2020-11-28 16:34:23 -0700

    mehtodsAssignmet

---
+++ methodsAssignment/methodsAssignment/Operations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace methodsAssignment
+{
+    class Operations
+    {
+        static public int addTwo(int input)
+        {
+            return input + 2;
+        }
+
+        static public int subTwo(int input)
+        {
+            return input - 2;
+        }
+
+        static public int divTwo(int input)
+        {
+            return input / 2;
+        }
+    }
+}
---
+++ methodsAssignment/methodsAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace methodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number would you like to perfrom calculations on? ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(input + " + 2 = " + Operations.addTwo(input));
+            Console.WriteLine(input + " - 2 = " + Operations.subTwo(input));
+            Console.WriteLine(input + " / 2 = " + Operations.divTwo(input));
+
+            Console.Read();
+        }
+    }
+}
