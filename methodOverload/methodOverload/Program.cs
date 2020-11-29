efebc855e1471b9dec2474b96997763ec7063f90
Cade Wilson <cade25wilson@gmail.com>
2020-11-28 17:28:12 -0700

    methodOverload

---
+++ methodOverload/methodOverload/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace ClassesLab114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+
+            /*call the first method, passing in an integer, such as 12. Then display the result to the screen.*/
+            Console.WriteLine(myClass.myMethod(12));
+
+            /*call the second method, passing in a decimal. Display the result to the screen.*/
+            Console.WriteLine(myClass.myMethod(1.5m));
+
+            /*call the third method, passing in a string that equates to an integer. 
+             * Display the result to the screen.*/
+            Console.WriteLine(myClass.myMethod("24"));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ methodOverload/methodOverload/newClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace methodOverload
+{
+    class newClass
+    {
+        public newClass()
+        {
+
+        }
+
+        public int myMethod(int num)
+        {
+            return num * 2;
+        }
+
+
+        public int myMethod(decimal d)
+        {
+            return Convert.ToInt32(d % 5);
+        }
+
+        public int myMethod(string s)
+        {
+            int final = Convert.ToInt32(s) + 1000;
+            return final;
+        }
+    }
+
+
+}
+ 
+ 
+ 
