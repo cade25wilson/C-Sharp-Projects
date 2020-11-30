061b5b165626fdd7bc3fc5d9491e316ffe4284fe
Cade Wilson <cade25wilson@gmail.com>
2020-11-30 02:31:56 -0700

    structs

---
+++ structs/structs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num1 = new Number();
+            num1.Amount = 443.32342m;
+
+            Console.WriteLine(num1.Amount);
+            Console.ReadLine();
+        }
+    }
+}
