b7a3c26b26cd27fa4e409f06949eb91a09507570
Cade Wilson <cade25wilson@gmail.com>
2020-11-29 22:05:52 -0700

    interface2

---
+++ interface2/interface2/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interface2
+{
+    public class Employee : Person, IQuittable
+    {
+        public void SayName()
+        {
+            base.SayName();
+        }
+
+        public void Quit(Employee employee)
+        {
+            Console.WriteLine(employee.FirstName +" " + employee.LastName + " Has quit this job");
+            Console.ReadLine();
+        }
+
+        
+    }
+}
---
+++ interface2/interface2/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interface2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.SayName();
+
+            IQuittable terminatedEmployee = new Employee();
+            terminatedEmployee.Quit(employee);
+
+        }
+    }
+}
