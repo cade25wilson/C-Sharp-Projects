ff8b072a16c4bfd6a7d3ffa6209d5a689406b38f
Cade Wilson <cade25wilson@gmail.com>
2020-11-29 18:48:41 -0700

    Method4

---
+++ Method4/Method4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Method4
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            //Telling the user to pick a number and passing in the operations class
+            Console.WriteLine("Choose a number:");
+            int userInput0 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Choose another number:");
+            int userInput1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+
+            operations.Divide(userInput0, userInput1);
+
+            Console.WriteLine();
+
+            operations.Divide(inputNum1: 4, inputNum2: 10);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Method4/Method4/operations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Method4
+{
+    public class operations
+    {
+        public static void Divide(int inputNum1, int inputNum2)
+        {
+            int product0 = inputNum1 / 2;
+            int input1 = inputNum2;
+
+            //taking user input dividing my two
+            Console.WriteLine("First number " + inputNum1 + ", divided by 2 = " + product0);
+            //saying what the second number they added and displaying it to console
+            Console.WriteLine("Second number is " + input1);
+
+        }
+    }
+}
