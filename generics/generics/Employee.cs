05edc28c04e0a97122f8169e878979b68b1afbad
Cade Wilson <cade25wilson@gmail.com>
2020-12-02 02:31:57 -0700

    added person class

--- generics/generics/Employee.cs
+++ generics/generics/Employee.cs
@@ -6,17 +6,8 @@ using System.Threading.Tasks;
 
 namespace generics
 {
-    class Employee<T> : Person
+    class Employee<T> 
     {
-
-        public int Id = 0;
         public List<T> Things = new List<T>();
-
-        //Calls the base constructor (Person)
-        public Employee()
-        {
-
-        }
-
     }
 }
--- generics/generics/Program.cs
+++ generics/generics/Program.cs
@@ -10,12 +10,12 @@ namespace generics
     {
         static void Main(string[] args)
         {
-            Employee<string> empString = new Employee<string>();
-            empString.Things.Add("first");
-            empString.Things.Add("second");
-            empString.Things.Add("third");
-            empString.Things.Add("fourth");
-            empString.Things.Add("fifth");
+            Employee<string> numString = new Employee<string>();
+            numString.Things.Add("first");
+            numString.Things.Add("second");
+            numString.Things.Add("third");
+            numString.Things.Add("fourth");
+            numString.Things.Add("fifth");
             Employee<int> intString = new Employee<int>();
             intString.Things.Add(1);
             intString.Things.Add(2);
@@ -23,9 +23,9 @@ namespace generics
             intString.Things.Add(4);
             intString.Things.Add(5);
 
-            Console.WriteLine("Employee.Thing of type string: ");
-            empString.Things.ForEach(x => { Console.WriteLine(x); });
-            Console.WriteLine("Employee.Thing of type int: ");
+            Console.WriteLine("Loop printing out all .things in string list ");
+            numString.Things.ForEach(x => { Console.WriteLine(x); });
+            Console.WriteLine("Loop printing out all .things in int list ");
             intString.Things.ForEach(x => { Console.WriteLine(x); });
 
             Console.Read();
