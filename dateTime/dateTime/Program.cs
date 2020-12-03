7f245988de48d6620817e18d560985bc974172b2
Cade Wilson <cade25wilson@gmail.com>
2020-12-03 08:51:35 -0700

    added exceptions

--- dateTime/dateTime/Program.cs
+++ dateTime/dateTime/Program.cs
@@ -10,22 +10,38 @@ namespace dateTime
     {
         static void Main(string[] args)
         {
-            DateTime currentTime = DateTime.Now;
-            Console.WriteLine(currentTime);
-            Console.WriteLine("Please enter a number");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            TimeSpan futureTime = new TimeSpan(num1, 0, 0);
-            DateTime combinedTime = currentTime.Add(futureTime);
-            if (num1 <= 1)
+                DateTime currentTime = DateTime.Now;
+                Console.WriteLine(currentTime);
+                Console.WriteLine("Please enter a number");
+            try { 
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                TimeSpan futureTime = new TimeSpan(num1, 0, 0);
+                DateTime combinedTime = currentTime.Add(futureTime);
+                if (num1 <= 1)
+                {
+                    Console.WriteLine("in " + num1 + " hour, the time will be " + combinedTime);
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("in " + num1 + " hours, the time will be " + combinedTime);
+                    Console.ReadLine();
+                }
+            }
+            catch (FormatException)
             {
-                Console.WriteLine("in " + num1 + " hour, the time will be " + combinedTime);
+                Console.WriteLine("Please type a number");
+                return;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
                 Console.ReadLine();
             }
-            else
-            {
-                Console.WriteLine("in " + num1 + " hours, the time will be " + combinedTime);
-                Console.ReadLine();
-            }
+
         }
     }
 }
