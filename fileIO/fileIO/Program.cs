87f37607295c3faeb969714954738aca54e906b6
Cade Wilson <cade25wilson@gmail.com>
2020-12-03 07:59:35 -0700

    File.IO

---
+++ fileIO/fileIO/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace fileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number here: ");
+            string num1 = Console.ReadLine();
+
+            using (StreamWriter file = new StreamWriter(@"test.txt", true))
+            {
+                file.Write(num1);
+            }
+
+            using (StreamReader file = new StreamReader("test.txt"))
+            {
+                Console.WriteLine("File Contents: ");
+                Console.WriteLine(file.ReadToEnd());
+            }
+
+            Console.Read(); 
+        }
+    }
+}
