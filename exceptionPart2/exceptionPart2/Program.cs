7752cd7cb5d7ceb3c97a5b30b92b1d2fae257972
Cade Wilson <cade25wilson@gmail.com>
2020-12-06 13:16:53 -0700

    exceptionPart2

---
+++ exceptionPart2/exceptionPart2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exceptionPart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Start:
+            Console.WriteLine("Please enter your age:");
+            int birthYear = 0;
+            //catching year that is negative or 0
+            try
+            {
+                birthYear = Convert.ToInt32(Console.ReadLine());
+                if (birthYear < 1)
+                    throw new InvalidOperationException();
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("Please do not enter a value lower than 1");
+                goto Start;
+            }
+            //catching inputs that are not numbers
+            catch (Exception)
+            {
+                Console.WriteLine("Please only enter digits.");
+                goto Start;
+            }
+            //subtracting birthyear from current date
+            Console.WriteLine("User was born in: " + DateTime.Now.AddYears(-birthYear).Year);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
