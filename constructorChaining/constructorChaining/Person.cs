91baaf7bb981324f285fdba51472fb92621931b8
Cade Wilson <cade25wilson@gmail.com>
2020-12-06 13:06:53 -0700

    constructorChaining

---
+++ constructorChaining/constructorChaining/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructorChaining
+{
+    class Person
+    {
+        string firstName;
+        string lastName;
+
+        public Person() : this("John", "Doe") { }
+
+        public Person(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + "[" + firstName + " " + lastName + "]");
+        }
+    }
+}
---
+++ constructorChaining/constructorChaining/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructorChaining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string constString = "This is constant.";
+            Console.WriteLine(constString);
+            var num = 3;
+            Console.WriteLine("variable num is of type: " + num.GetType());
+            var str = "Is a string.";
+            Console.WriteLine("variable str is of type: " + str.GetType());
+
+            Person person = new Person();
+            person.SayName();
+            Console.ReadLine();
+
+        }
+    }
+}
